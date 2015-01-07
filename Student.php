89cba1e1e9e109973cf05b5e44a3f2d1f1903307
maecst <mae.cst@hotmail.com>
2015-01-07 10:37:39 -0800

    Added Student class

--- Student.php
+++ Student.php
@@ -27,21 +27,28 @@ class Student {
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
-    
+
+    public function average() {
+        $total = 0;
+
+        foreach ($this->grades as $value) {
+            $total += $value;
+        }
+
+        return $total / count($this->grades);
+    }
+
     function toString() {
-        $result = $this->first_name.''.$this->surname;
-        $result = '('.$this->average().")\n";
+        $result = $this->first_name . ' ' . $this->surname;
+        $result = '(' . $this->average() . ")\n";
         
         foreach ($this->emails as $which => $what) {
             $result .= $which . ':' . $what . "\n";
         }
         $result .= "\n";
         
-        return '<pre>'.$result.'</pre>';
+        return '<pre>' . $result . '</pre>';
     }
-    
-    
-    
-    
-    
+
+
 }
